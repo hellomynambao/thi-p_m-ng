e064513ea4132f7ac6eb63d4e7148d7c97aea8a0
hellomynambao <151444200+hellomynambao@users.noreply.github.com>
2023-11-19 21:01:37 +0700

    thiệp_mừng

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'teacher';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc mừng ngày nhà giáo Việt Nam';
+const titleCard = 'Tặng thầy cô';
+const contentCard = 'Chúc thầy cô lun vui vẻ và hạnh phúc. Luôn đạt được nhiều thành công. Nhớ bao em trà sữa nha';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'tiền.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
